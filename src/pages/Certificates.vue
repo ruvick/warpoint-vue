8532f46882d27b24a95ffc088f63a16610cf3901
ruvick <rudikov.web@ya.ru>
2024-06-20 18:14:02 +0300

    Структура таблицы

--- src/pages/Certificates.vue
+++ src/pages/Certificates.vue
@@ -75,7 +75,7 @@
 									<div class="row col bg-dark-box q-pt-md q-pb-md q-pl-lg q-pr-lg">
 
 										<!-- Блок 1 Загрузка сертификата  -->
-										<div class="column items-center justify-center" style="width: 100%; display: none;">
+										<div class="column items-center justify-center" style="width: 100%; display: non;">
 											<div class="row">
 												<div class="row items-center justify-center text-h6 text-grey-10">Здесь появится информация о сертификате
 													<i class="q-icon text-grey-10 q-ml-sm" aria-hidden="true" role="presentation" style="font-size: 24px;">
@@ -394,7 +394,20 @@
 									<div class="text-h6 q-mb-xl">История активаций подарочных сертфикатов</div>
 								</div>
 
-								<q-btn label="Close Icon" @click="icon = true" />
+
+								<q-table
+									style="height: 400px"
+									flat bordered
+									:rows="rows"
+									:columns="columns"
+									row-key="index"
+									virtual-scroll
+									v-model:pagination="pagination"
+									:rows-per-page-options="[0]"
+								/>
+
+								
+								<div class="gghk" @click="icon = true">Открыть модалку</div>
 
 								<div class="activ-history row items-center q-mt-md">
 									<div class="activ-history__item row items-center q-mr-xl">
@@ -403,6 +416,7 @@
 									<div class="activ-history__item text-h6">50 000 ₽ </div>
 								</div>
 
+								<!-- Popup  -->
 								<q-dialog v-model="icon"> 
 									<q-card class="q-card-activ bg-blue-grey-9 q-pt-xs q-pb-xl" style="max-width: 832px; width: 100%;">
 									<q-card-section class="row items-center q-pl-xl">
@@ -470,15 +484,158 @@
 <script>
 import { ref } from 'vue';
 
+const columns = [
+  {
+    name: 'index',
+    label: '№',
+    field: 'index'
+  },
+  {
+    name: 'name',
+    required: true,
+    label: 'Dessert (100g serving)',
+    align: 'left',
+    field: row => row.name,
+    format: val => `${val}`,
+    sortable: true
+  },
+  { name: 'calories', align: 'center', label: 'Calories', field: 'calories', sortable: true },
+  { name: 'fat', label: 'Fat (g)', field: 'fat', sortable: true },
+  { name: 'carbs', label: 'Carbs (g)', field: 'carbs' },
+  { name: 'protein', label: 'Protein (g)', field: 'protein' },
+  { name: 'sodium', label: 'Sodium (mg)', field: 'sodium' },
+  { name: 'calcium', label: 'Calcium (%)', field: 'calcium', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) },
+  { name: 'iron', label: 'Iron (%)', field: 'iron', sortable: true, sort: (a, b) => parseInt(a, 10) - parseInt(b, 10) }
+]
+
+const seed = [
+  {
+    name: 'Frozen Yogurt',
+    calories: 159,
+    fat: 6.0,
+    carbs: 24,
+    protein: 4.0,
+    sodium: 87,
+    calcium: '14%',
+    iron: '1%'
+  },
+  {
+    name: 'Ice cream sandwich',
+    calories: 237,
+    fat: 9.0,
+    carbs: 37,
+    protein: 4.3,
+    sodium: 129,
+    calcium: '8%',
+    iron: '1%'
+  },
+  {
+    name: 'Eclair',
+    calories: 262,
+    fat: 16.0,
+    carbs: 23,
+    protein: 6.0,
+    sodium: 337,
+    calcium: '6%',
+    iron: '7%'
+  },
+  {
+    name: 'Cupcake',
+    calories: 305,
+    fat: 3.7,
+    carbs: 67,
+    protein: 4.3,
+    sodium: 413,
+    calcium: '3%',
+    iron: '8%'
+  },
+  {
+    name: 'Gingerbread',
+    calories: 356,
+    fat: 16.0,
+    carbs: 49,
+    protein: 3.9,
+    sodium: 327,
+    calcium: '7%',
+    iron: '16%'
+  },
+  {
+    name: 'Jelly bean',
+    calories: 375,
+    fat: 0.0,
+    carbs: 94,
+    protein: 0.0,
+    sodium: 50,
+    calcium: '0%',
+    iron: '0%'
+  },
+  {
+    name: 'Lollipop',
+    calories: 392,
+    fat: 0.2,
+    carbs: 98,
+    protein: 0,
+    sodium: 38,
+    calcium: '0%',
+    iron: '2%'
+  },
+  {
+    name: 'Honeycomb',
+    calories: 408,
+    fat: 3.2,
+    carbs: 87,
+    protein: 6.5,
+    sodium: 562,
+    calcium: '0%',
+    iron: '45%'
+  },
+  {
+    name: 'Donut',
+    calories: 452,
+    fat: 25.0,
+    carbs: 51,
+    protein: 4.9,
+    sodium: 326,
+    calcium: '2%',
+    iron: '22%'
+  },
+  {
+    name: 'KitKat',
+    calories: 518,
+    fat: 26.0,
+    carbs: 65,
+    protein: 7,
+    sodium: 54,
+    calcium: '12%',
+    iron: '6%'
+  }
+]
+
+// we generate lots of rows here
+let rows = []
+for (let i = 0; i < 1000; i++) {
+  rows = rows.concat(seed.slice(0).map(r => ({ ...r })))
+}
+rows.forEach((row, index) => {
+  row.index = index
+})
+
 export default {
      name: 'Certificates',
+	  
   setup () {
     return {
       tab: ref('mails'),
 		value: 71,
 		certificateNumber: ref(''),
 		pin: ref(''),
-		icon: ref(false)
+		icon: ref(false),
+		columns,
+      rows,
+
+      pagination: ref({
+        rowsPerPage: 0
+      })
     }
   }
 };
